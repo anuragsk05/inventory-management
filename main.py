af926b30de26fe1322b5db89b295b7e534dc9765
Montek <montekj123@gmail.com>
2025-06-16 23:18:40 -0400

    Add working main.py with add and delete functionality

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import models
+import models, json
 import sqlalchemy as db
 from fastapi import FastAPI, Request
 from database import SessionLocal, engine
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from models import Item
+import sqlite3
+
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
@@ -22,32 +24,72 @@ def add():
     c = str(input("Enter the brand of the item: "))
     d = str(input("Enter the date of this added item: "))
     e = int(input("Enter the amount of your item you would like to add: "))
-    return a, b, c, d, e
-    
-#call add function
-a, b, c, d, e = add()
 
-    #will return user input
+    #assigns user inputs to objects within class
+    return Add(id=a, item_name=b, brand=c, date_added=d, quantity=e)
+
+def delete():
+    # Connect to the database
+    conn = sqlite3.connect('items.db')
+    cur = conn.cursor()
+
+    item_id = int(input("Enter the ID number of the item you want to delete: "))
+
+    cur.execute("SELECT * FROM items WHERE id = ?", (item_id,))
+    result = cur.fetchone()
+
+    if result:
+        cur.execute("DELETE FROM items WHERE id = ?", (item_id,))
+        conn.commit()
+        print("Item deleted successfully.")
+    conn.close()
+
 inventory = {}
-call = Add(id=a, item_name=b, brand=c, date_added=d, quantity=e)
+def main():
+    choice = int(input("Enter 1 if you want to add something to database, enter 2 if you want to delete something from it: "))
+    if choice == 1:
+        item_data = add()
 
-#new key assigned with value from called function
-#id number is whats will be connected to all other properties
-inventory[a] = call
+        #inventory will have a key thats added via object id to be assigned to attributes within add function
+        inventory[item_data.id] = item_data
 
+        # create a table based off methods of objects develpoed within "class Item"
+        item = Item(
+            id = item_data.id,
+            item_name = item_data.item_name,
+            brand = item_data.brand,
+            date_added = item_data.date_added,
+            quantity = item_data.quantity
+        )
 
-def deletion():
-    key_del = int(input("Enter an the numnber of the id you want to delete"))
-    if key_del in inventory:
-        inventory.pop(key_del)
+        # open and insert session
+        db_session = SessionLocal()
+        db_session.add(item)
+        db_session.commit()
 
-#calls instance of object to be used
-item = Item(id=a, item_name=b, brand=c, date_added=d, quantity=e)
+        # Retrieve all items and export to JSON
+        items = db_session.query(Item).all()
+        convert_list = [{
+            "id": i.id,
+            "item_name": i.item_name,
+            "brand": i.brand,
+            "date_added": i.date_added,
+            "quantity": i.quantity
+        } for i in items]
 
-#to call sqlite database
-db_session = SessionLocal()
+        with open("storage.json", "w") as outfile:
+            json.dump(convert_list, outfile, indent=4)
+        db_session.close()
 
-#method to add to database via sqlalchemy to later write into it and then close it
-db.add(item)
-db.commit()
-db.close()
+        with open('storage.json') as file:
+            data = json.load(file)
+        printer = json.dumps(data, indent=4)
+        print(printer)
+
+    if choice == 2:
+        delete()
+
+main()
+
+if __name__ == "__main__":
+    main()
